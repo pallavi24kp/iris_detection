5b7ebbe136a88203a17059b5db13c5a98fb68806
PALLAVI <kppallavi712@gmail.com>
2025-09-06 16:14:23 +0530

    completed

--- iris_detection.py
+++ iris_detection.py
@@ -8,9 +8,37 @@ df=pd.DataFrame(data=iris.data,columns=iris.feature_names)
 df['species']=iris.target
 print(df.head())
 
-#Scatter plot : Sepal Length vs Sepal Width
-plt.scatter=(df['sepal length (cm)'],df['sepal width (cm)'],c=df['species'])
-plt.xlabel('Sepal Length (cm)')
-plt.ylabel('Sepal Width (cm)')
-plt.title('Iris Sepal Length vs Width')
-plt.show()
+#Scatter plot : Sepal Length vs Sepal Width (comment this to check the accuracy)
+#plt.scatter(df['sepal length (cm)'],df['sepal width (cm)'],c=df['species'])
+#plt.xlabel('Sepal Length (cm)')
+#plt.ylabel('Sepal Width (cm)')
+#plt.title('Iris Sepal Length vs Width')
+#plt.show()
+
+from sklearn.model_selection import train_test_split
+
+#Features and Labels
+X=df.iloc[:,:-1]
+y=df['species']
+
+#Train-Test Split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+
+#Create and Train Model
+model=LogisticRegression(max_iter=200)
+model.fit(X_train,y_train)
+
+from sklearn.metrics import accuracy_score,classification_report
+
+#Making predictions
+y_pred=model.predict(X_test)
+print("Accuracy:",accuracy_score(y_test,y_pred),flush=True)
+print("Classification Report:\n",classification_report(y_test,y_pred,target_names=iris.target_names))
+
+#Predict new sample
+import pandas as pd
+sample=pd.DataFrame([[5.1,3.5,1.4,0.2]],columns=iris.feature_names)
+prediction=model.predict(sample)
+print("Predicted Species :",iris.target_names[prediction[0]])
